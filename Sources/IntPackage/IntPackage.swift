4dd92bce30b351f4478258ca354c1ca035f7b485
Rupak Shakya <ingrails.rupak@gmail.com>
2024-12-31 14:05:47 +0545

    initial commit

---
+++ Sources/IntPackage/IntPackage.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public class Greeting {
+    
+    public init() { }
+    
+    public func greetUser() -> String {
+        return "Hello user, i am greeting you from swift"
+    }
+}
